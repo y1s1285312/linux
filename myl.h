0331aaeec98452b7ad26088b32b14b067c8657df
y1s1285312 <70126240+y1s1285312@users.noreply.github.com>
2020-08-25 14:23:01 +0900

    Update myl.h

--- myl.h
+++ myl.h
@@ -1,9 +1,24 @@
-#include<stdio.h>
-#include<sys/types.h>
 #include<unistd.h>
 #include<stdlib.h>
 #include<sys/wait.h>
 #include<signal.h>
 #include<wait.h>
+#include<string.h>
+#include<errno.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<sys/shm.h>
+
+
 
 #define print printf
+#define KEY_VALUE (key_t)1234
+#define MAX_TEXT 512
+
+struct my_msg_st
+{
+    long int my_msg_type;
+    unsigned long int total;
+    char some_text[BUFSIZ];
+
+};
